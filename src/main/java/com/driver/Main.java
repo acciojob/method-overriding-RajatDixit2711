b223a73cc85695dbbf6ca91959ba7f882a816fc3
Rajat Dixit <Rajatdixithbtu@gmail.com>
2023-06-16 14:18:30 +0530

    31

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,26 +5,30 @@ import java.sql.SQLOutput;
 public class Main {
 
 
-    public static void main(String[] args) {
+     public static class  A
+    {
+        public  String meth()
+        {
+            return "Invoking method from class A";
+        }
+
+    }
+   public static class  B extends A
+    {
+
+        public  String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+
+        public static void main(String[] args) {
     B b= new B();
             b.meth();
 
+            B b1= new B();
+            b1.meth();
+
     }
 }
-  class  A
- {
-        public  String meth()
-         {
-             return "Invoking method from class A";
-         }
-
- }
-class  B extends A
-{
-
-    public  String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
 
 }
